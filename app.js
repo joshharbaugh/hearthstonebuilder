6af8c5efc12c3a114ec7b6930fc9e3d40ba472d0
Josh Harbaugh <josh.harbaugh@yougov.com>
2013-09-29 16:10:20 -0400

    update raven config.

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(express.cookieSession({secret: 'dfj3fk2i3lkjfsld92492kc0!fkjdf0249fk29#4
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(app.router);
-app.use(raven.middleware.express('https://7907b957d0e44be88b4464c41fd47826:8d791509cb3f453bbba8826bd1b1fb04@app.getsentry.com/13116'));
+app.error(raven.middleware.express('https://62b9f8a1d863427f9425e95e31051712:3e8ebb29977f4325b8b9fe34a328b137@app.getsentry.com/13667'));
 
 // development only
 if ('development' == app.get('env')) {
