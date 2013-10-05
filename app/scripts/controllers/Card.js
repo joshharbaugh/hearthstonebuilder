1e36722ff56907cb14e5d4c75e972a675f027497
Josh Harbaugh <josh.harbaugh@yougov.com>
2013-10-04 22:16:31 -0400

    update cards templates

--- app/scripts/controllers/Card.js
+++ app/scripts/controllers/Card.js
@@ -1,25 +1,25 @@
 angular.module('hsbApp.CardControllers', [])
 
-	.controller('CardCtrl',['$scope','allCards','abilityCards','heroCards','heroPowerCards','minionCards','weaponCards','$stateParams', function ($scope, allCards, abilityCards, heroCards, heroPowerCards, minionCards, weaponCards, $stateParams) {
+    .controller('CardCtrl',['$scope','allCards','abilityCards','heroCards','heroPowerCards','minionCards','weaponCards','$stateParams', function ($scope, allCards, abilityCards, heroCards, heroPowerCards, minionCards, weaponCards, $stateParams) {
 
-		$scope.cards = {};
-		$scope.cards.all = allCards;
-		$scope.cards.ability = abilityCards;
-		$scope.cards.hero = heroCards;
-		$scope.cards.heroPower = heroPowerCards;
-		$scope.cards.minion = minionCards;
-		$scope.cards.weapon = weaponCards;
+        $scope.cards = {};
+        $scope.cards.all = allCards;
+        $scope.cards.ability = abilityCards;
+        $scope.cards.hero = heroCards;
+        $scope.cards.heroPower = heroPowerCards;
+        $scope.cards.minion = minionCards;
+        $scope.cards.weapon = weaponCards;
 
-		$scope.$watch('cards', function(newVal, oldVal) {
-			if(newVal) {
-				$scope.cards = newVal;
-			}
-		});
+        $scope.$watch('cards', function(newVal, oldVal) {
+            if(newVal) {
+                $scope.cards = newVal;
+            }
+        });
 
-		$scope.addCard = function() {
-			$scope.$parent.$broadcast('addCard', {data:this.card});
-		};
+        $scope.addCard = function() {
+            $scope.$parent.$broadcast('addCard', {data:this.card});
+        };
 
-		$scope.onReady();
+        $scope.onReady();
 
-	}]);
+    }]);
