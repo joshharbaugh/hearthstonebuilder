62a7d8f294353d9c9c19a52f56a093b4bd7138dc
Josh Harbaugh <pixelhavenllc@gmail.com>
2016-12-22 16:30:31 -0500

    Create slug filter.

--- app/scripts/filters/appFilters.js
+++ app/scripts/filters/appFilters.js
@@ -4,4 +4,15 @@ angular.module('hsbApp.Filters', [])
         return function(input) {
             return input.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
         };
+    })
+
+    .filter('slug', function() {
+        return function(input) {
+            return input.toString().toLowerCase()
+              .replace(/\s+/g, '-')
+              .replace(/[^\w\-]+/g, '')
+              .replace(/\-\-+/g, '-')
+              .replace(/^-+/, '')
+              .replace(/-+$/, '')
+        }
     });
